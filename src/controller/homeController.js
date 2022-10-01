cd1772c25bf72ee69e68b941efd408658f229f49
unknown <Tuan Huy@MSI.com>
2022-10-01 18:44:45 +0700

    vide 13 finish CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -38,6 +38,24 @@ let createNewUser = async (req, res) => {
     await pool.execute('insert into users(firstName, lastName, email, address) values (?, ?, ?, ?)',[firstName, lastName, email, address]);
     // khi thực hiện xong sẽ trở về trang ban đầu luôn và thêm vào màn hình
     return res.redirect('/');
-}
+};
 
-module.exports = { getHomePage, getDetailPage, createNewUser };
+let deleteUser = async (req, res) => {
+    let userId = req.body.userId;
+    await pool.execute('delete from users where id = ?', [userId]);
+    return res.redirect('/');
+};
+
+let getEditPage = async (req, res) => {
+    let id = req.params.id;
+    let [user] = await pool.execute('Select * from users where id = ?', [id])
+    return res.render('update.ejs', { dataUser: user[0] });
+};
+
+let postUpdateUser = async (req, res) => {
+    let { firstName, lastName, email, address, id } = req.body;
+    await pool.execute('update users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?', [firstName, lastName, email, address, id]);
+    return res.redirect('/');
+};
+
+module.exports = { getHomePage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser };
--- src/route/web.js
+++ src/route/web.js
@@ -7,6 +7,9 @@ const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/detail/user/:id', homeController.getDetailPage);   // Dấu : đùng query params
     router.post('/create-new-user', homeController.createNewUser);
+    router.post('/delete-user', homeController.deleteUser);
+    router.get('/edit-user/:id', homeController.getEditPage);
+    router.post('/update-user', homeController.postUpdateUser)
     app.get('/home', (req, res) => {
         res.send('HELLO');
     });
